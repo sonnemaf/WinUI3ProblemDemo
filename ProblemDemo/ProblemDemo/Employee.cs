782c4d9457d88ec43c6a3c9925eb8c0f62738319
sonnemaf <fons.sonnemans@reflectionit.nl>
2021-08-25 13:31:54 +0200

    Add project files.

---
+++ ProblemDemo/ProblemDemo/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemDemo {
+    public class Employee {
+        public Employee(string name, double salary) {
+            this.Name = name;
+            this.Salary = salary;
+        }
+
+        public string Name { get; set; }
+        public double Salary { get; set; }
+    }
+}
